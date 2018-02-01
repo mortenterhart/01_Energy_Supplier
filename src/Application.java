eae5b1810fc4352b1e862506bd8477711bcb5c73
Philipp Seitz <seitzphilipp22@gmail.com>
2018-02-01 11:18:35 +0100

    First Lambda Query and first method implemented

--- src/Application.java
+++ src/Application.java
@@ -90,7 +90,7 @@ public class Application {
     }
 
     public static void main(String... args) {
-    Application app = new Application();
-    app.execute();
+        Application app = new Application();
+        app.execute();
     }
 }
